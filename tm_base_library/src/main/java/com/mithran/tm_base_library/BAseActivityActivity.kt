c0369d4931bd0e0a8f977b0e199ce3250b792860
tmoorthymca <tmoorthymca@github.com>
2018-08-06 23:15:21 +0530

    create demo lib

---
+++ tm_base_library/src/main/java/com/mithran/tm_base_library/BAseActivityActivity.kt
@@ -0,0 +1,12 @@
+package com.mithran.tm_base_library
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BAseActivityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base_activity)
+    }
+}
